6541d1c93acde8d4de8ed0aec557307a488d3727
Jesus Almaral <almaral17@gmail.com>
2020-07-29 21:57:31 -0500

    Add capability to take photos

--- app/src/main/java/com/hackaprende/registrodesuperheroes/MainActivity.kt
+++ app/src/main/java/com/hackaprende/registrodesuperheroes/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.hackaprende.registrodesuperheroes
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
 import com.hackaprende.registrodesuperheroes.databinding.ActivityMainBinding
 
+private const val PHOTO_REQUEST_CODE = 2000
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var superheroImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -19,11 +27,31 @@ class MainActivity : AppCompatActivity() {
             val hero = Hero(heroName, alterEgoName, bio, power)
             openDetailsActivity(hero)
         }
+
+        superheroImage = binding.superheroImage
+        superheroImage.setOnClickListener {
+            openCamera()
+        }
+    }
+
+    private fun openCamera() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, PHOTO_REQUEST_CODE)
     }
 
     private fun openDetailsActivity(hero: Hero) {
         val intent = Intent(this, DetailActivity::class.java)
         intent.putExtra(DetailActivity.HERO_KEY, hero)
         startActivity(intent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == PHOTO_REQUEST_CODE) {
+            val extras = data?.extras
+            val bitmap = extras?.get("data") as Bitmap
+            superheroImage.setImageBitmap(bitmap)
+        }
     }
 }
