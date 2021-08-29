7c5b0e9fc70ce7b233824b25c44c5d67ef6eca04
Jesus Valdez <hackaprende@gmail.com>
2021-08-29 12:32:55 -0500

    Update project dependencies and configurations

--- app/src/main/java/com/hackaprende/registrodesuperheroes/Hero.kt
+++ app/src/main/java/com/hackaprende/registrodesuperheroes/Hero.kt
@@ -1,7 +1,7 @@
 package com.hackaprende.registrodesuperheroes
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class Hero(val name: String, val alterEgo: String, val bio: String, val power: Float) : Parcelable
